48ffd4b52ce5d20d1d84d740c337eaaf6ffb5347
techhype <kishore.a.175@gmail.com>
2022-06-13 10:15:19 +0530

    Add save-details form route logic

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // Imports
 const express = require('express');
 const app = express();
+const db = require('./config/database');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const formRoutes = require('./routes/formRoutes');
 const dotenv = require('dotenv');
 const fetch = require('node-fetch');
 
@@ -10,24 +12,11 @@ const fetch = require('node-fetch');
 dotenv.config({ path: './.env'});
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(formRoutes);
 
 // ENV variables
 const hostName =process.env.HOSTNAME;
 const PORT = process.env.PORT;
-
-app.get('/',(req,res)=>{
-  // var v = [];
-  fetch("https://api.coinranking.com/v1/public/coins?ids=1,2,3")
-    .then((response) => response.json())
-    .then((json) => {
-      (json.data.coins).map(coin => {
-        const {name,price,change} = coin;
-        console.log({name,price,change});
-      });
-    })
-    .catch((error) => console.error(error));
-    // console.log(v);
-});
 
 // //Create Database
 // app.get('/createdb',(req,res)=>{
@@ -44,6 +33,18 @@ app.get('/',(req,res)=>{
 //             (id int AUTO_INCREMENT, username VARCHAR(255), 
 //             email VARCHAR(255), password VARCHAR(255),
 //             mnumber VARCHAR(20), PRIMARY KEY(id))`;
+//   db.query(sql,(err,result)=>{
+//     if(err) throw err;
+//     res.send('Table created');
+//   });
+// });
+
+// // Create Form Table
+// app.get('/createFormTable',(req,res)=>{
+//   let sql = `CREATE TABLE form (id VARCHAR(255), firstname VARCHAR(255),
+//             lastname VARCHAR(255), middlename VARCHAR(255),
+//             address VARCHAR(255), email VARCHAR(255), 
+//             phonenumber VARCHAR(255), height DOUBLE, weight DOUBLE, PRIMARY KEY(id))`;
 //   db.query(sql,(err,result)=>{
 //     if(err) throw err;
 //     res.send('Table created');
--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,30 +1,34 @@
 const express = require('express'); 
-const db = require('../database');
+const db = require('../config/database');
+const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
 
 router.post('/form/save-details',async(req,res)=>{
   const { firstname, lastname, middlename, address, email, phonenumber, height, weight } = req.body;
-  db.query('SELECT id FROM formdetails WHERE =? ', [username], async (err, result) => {
+  
+  console.log(firstname, lastname, middlename, address, email, phonenumber, height, weight)
+  db.query('SELECT email FROM form WHERE email=? ', [email], async (err, result) => {
     if(err){
       console.log(err);
       return;
     }
     if (result.length > 0) {
-      res.json({error:'Username already in use!!'});
+      res.json({status: '500',error:'Form already submitted with same email'});
       return;
     }
-    let hashedPassword = await bcrypt.hash(password, 8);
-    let sql = 'INSERT INTO user SET ?';
-    db.query(sql, { username, password: hashedPassword }, async (err, result) => {
-      if (err) throw err;
+    const id = uuidv4().slice(0,8);
+    let sql = 'INSERT INTO form SET ?';
+    db.query(sql, { id, firstname, lastname, middlename, address, email, phonenumber, height, weight }, async (err, result) => {
+      if (err){
+        res.json({status: '500',error: 'Error occured in saving form details'});
+        throw err;
+      } 
       console.log(`Row inserted successfully ${result}`);
-      // res.json({message:'Created an Account'});  
-      const id = result.insertId;
-      const token = await jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
-      });
-      res.json({token,username});
+      // res.json({message:'Created an Account'});       
+      res.json({id,status: '200',message: 'Added Form details successfully'});
     });
   }); 
 });
+
+module.exports = router;
